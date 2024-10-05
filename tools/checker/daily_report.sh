49b304b2d11e35b3cc465f46c606fc3203614072
smilestar13 <117439849+smilestar13@users.noreply.github.com>
2024-10-05 16:41:06 +0300

    Dawn worker update

--- tools/checker/daily_report.sh
+++ tools/checker/daily_report.sh
@@ -8,15 +8,15 @@ function colors {
 }
 
 function logo {
-  curl -s https://raw.githubusercontent.com/smilestar13/nodes/main/tools/logo/smilestar.sh | bash
+  curl -s https://raw.githubusercontent.com/smilestar13/nodes/main/tools/logo/smilestar.sh | bash | tee -a $LOG_FILE
 }
 
 function line_1 {
-  echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}"
+  echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}" | tee -a $LOG_FILE
 }
 
 function line_2 {
-  echo -e "${RED}##############################################################################${NORMAL}"
+  echo -e "${RED}##############################################################################${NORMAL}" | tee -a $LOG_FILE
 }
 
 function start_work {
@@ -48,7 +48,6 @@ function waku_worker {
   sleep 1000
 }
 
-
 function og_worker { 
   echo "Запуск проверки og_checker.yml в $(date '+%d-%m-%Y %H:%M:%S')" >> $LOG_FILE
   ansible-playbook -i inventory og_check_sync.yml >> $LOG_FILE 2>&1
@@ -57,6 +56,18 @@ function og_worker {
       echo "og_checker.yml выполнен успешно в $(date '+%d-%m-%Y %H:%M:%S')" >> $LOG_FILE
   else
       echo "Ошибка при выполнении og_checker.yml в $(date '+%d-%m-%Y %H:%M:%S')" >> $LOG_FILE
+  fi
+  sleep 1000
+}
+
+function dawn_worker { 
+  echo "Запуск проверки dawn_worker.yml в $(date '+%d-%m-%Y %H:%M:%S')" >> $LOG_FILE
+  ansible-playbook -i inventory dawn_checker.yml >> $LOG_FILE 2>&1
+
+  if [ $? -eq 0 ]; then
+      echo "dawn_worker.yml выполнен успешно в $(date '+%d-%m-%Y %H:%M:%S')" >> $LOG_FILE
+  else
+      echo "Ошибка при выполнении dawn_worker.yml в $(date '+%d-%m-%Y %H:%M:%S')" >> $LOG_FILE
   fi
   sleep 1000
 }
@@ -77,6 +88,8 @@ line_1
 waku_worker
 line_1
 og_worker
+line_1
+dawn_worker
 line_2
 end_work
 line_2
