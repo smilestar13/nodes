9fab28f1974e54d5fa6e979d56b416ab92e8beec
smilestar13 <117439849+smilestar13@users.noreply.github.com>
2024-10-09 16:31:52 +0300

    Update hemi_install.sh

--- hemi/hemi_install.sh
+++ hemi/hemi_install.sh
@@ -44,25 +44,24 @@ function new_wallet {
 function start_mainer {
   echo "Mainer setup"
   sudo tee /etc/systemd/system/hemi.service > /dev/null <<EOF
-  [Unit]
-  Description=Hemi miner
-  After=network.target
-  
-  [Service]
-  User=root
-  Environment="POPM_BTC_PRIVKEY=$PRIVATE_KEY"
-  Environment="POPM_STATIC_FEE=250"
-  Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
-  WorkingDirectory=/root/heminetwork
-  ExecStart=/root/heminetwork/popmd
-  Restart=on-failure
-  RestartSec=10
-  LimitNOFILE=65535
-  
-  [Install]
-  WantedBy=multi-user.target
-  EOF
+[Unit]
+Description=Hemi miner
+After=network.target
 
+[Service]
+User=root
+Environment="POPM_BTC_PRIVKEY=$PRIVATE_KEY"
+Environment="POPM_STATIC_FEE=250"
+Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
+WorkingDirectory=/root/heminetwork
+ExecStart=/root/heminetwork/popmd
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
   sudo systemctl enable hemi &>/dev/null
   sudo systemctl daemon-reload
   sudo systemctl start hemi
